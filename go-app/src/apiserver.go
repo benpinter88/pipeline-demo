3971888ebcc2fa039c470d9a2d619a935d633ec4
benpinter88 <50327569+benpinter88@users.noreply.github.com>
2022-12-07 17:57:34 -0500

    initial commit

---
+++ go-app/src/apiserver.go
@@ -0,0 +1,21 @@
+package main
+// Importing the required packages
+import (
+	"fmt"
+	"net/http"
+	"log"
+)
+// Defining the messageHandler function to handle the requests
+func messageHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Response from Go server")
+}
+// Defining the main function to start the server
+func main() {
+	server := http.Server {
+		Addr: ":8080",
+	}
+// Registering the messageHandler function to handle the requests on ip:8080/message path
+	http.HandleFunc("/message", messageHandler)
+	log.Println("Starting server at port 8080")
+	server.ListenAndServe()
+}
