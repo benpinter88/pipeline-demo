160c33d53900547554fdaf19a8e55d15a9363a07
benpinter88 <50327569+benpinter88@users.noreply.github.com>
2022-12-08 11:26:28 -0500

    test commit

--- go-app/src/apiserver.go
+++ go-app/src/apiserver.go
@@ -16,6 +16,6 @@ func main() {
 	}
 // Registering the messageHandler function to handle the requests on ip:8080/message path
 	http.HandleFunc("/message", messageHandler)
-	log.Println("Starting server at port 8080")
+	log.Println("Starting server on port 8080")
 	server.ListenAndServe()
 }
